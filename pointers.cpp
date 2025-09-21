a1e01cb78e8c4eef8d5bf1ff253714aa29c8494d
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-21 13:59:55 -0700

    Second addition to pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -2,12 +2,44 @@
 #include <string>
 usding namespace std;
 
+struvt Player {
+    string name; //player name
+    int level;  //player lvl
+    string inventory;  //items
+    int itemCount;  //number of items in inventory
+
+}
+
 
 int main() {
+    Party myParty;
 
+    //adding players
+    Player p1("ARcher")
+    p1.addItem("Bow")
+    p1.addItem("Health potion")
 
+    Plater p2("Mage")
+    p2.addItem("staff")
+    p2.addItem("Grimore")
+    p2.addItem("mana potion")
 
+    PLayer p3("Warrior")
+    p3.addItem("sword")
+    p3.addIrem("stamina potion")
 
+    Plater p4("tank")
+    p4.addItem("shield")
+    p4.addItem("strength potion")
+
+    //add players to a party
+    myParty.addPlayer(p1);
+    myParty.addPlayer(p2);
+    myParty.addPlayer(p3)
+    myParty.addPlayer(p4);
+
+    //output full party
+    myParty.printParty();
 
     return 0;
 }
