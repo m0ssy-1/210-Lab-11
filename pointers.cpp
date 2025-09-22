f4df13aeb3892bb4ea6823dd22b7b2abcd595397
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-21 17:34:27 -0700

    Seventh addition to pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -5,27 +5,31 @@ using namespace std;
 struct Player {
     string name; //player name
     int level;  //player lvl
-    string inventory;  //items
+    string* inventory;  //items
     int itemCount;  //number of items in inventory
 
     //constructor
     Player(string n = "", int lvl = 1) {
         name = n;
         level = lvl;
-        inventory = nullptr;
+        inventory = nullptr;  //no items
         itemCount = 0;
     }
 
     //add item to inv
-    void addItem(string item){
+    void addItem(string item) {
+
         string* newInventory = new string[itemCount + 1];
 
         for (int i = 0; i < itemCount; i++) {
             newInventory[i] = inventory[i];
         }
-        newInventory(itemCount) = item;
 
-        inventory = new newInventory;
+        newInventory[itemCount] = item;
+
+        delete[] inventory;
+
+        inventory = newInventory;
         itemCount++;
 
     }
@@ -35,13 +39,22 @@ void printPlayer() {
     cout << "something" << name << " something" << level << "something" ;
     cout << "something" ;
     if(itemCount == 0) {
-        cout << ""
+        cout << "";
+    } else {
+        for (int i = 0; i < itemCount; i++){
+            cout << inventory[i];
+            if (i < itemCount - 1) cout << "  ";
+        }
+        cout << endl;
     }
 }
+    Player() {
+        delete[] inventory;
+    }
 
+};
 
-}
-//ass player to party
+//add player to party
 struct Party {
     Player* players;
     int playerCount;
@@ -52,18 +65,20 @@ struct Party {
     }
 
     void addPlayer(Player newPlayer) {
+
         Player* newPlayers = new Player[playerCount + 1];
-        for (int i = 0; i < playerCount; i++)[
+        for (int i = 0; i < playerCount; i++) {
             newPlayers[i] = players[i];
-        ]
+        }
+
         newPlayers[playerCount] = newPlayer;
 
         delete[] players;
         players = newPlayers;
-        playerCount;
+        playerCount++;
     }
 
-}
+};
 
 
 
@@ -71,27 +86,27 @@ int main() {
     Party myParty;
 
     //adding players
-    Player p1("ARcher")
-    p1.addItem("Bow")
-    p1.addItem("Health potion")
+    Player p1("ARcher");
+    p1.addItem("Bow");
+    p1.addItem("Health potion");
 
-    Plater p2("Mage")
-    p2.addItem("staff")
-    p2.addItem("Grimore")
-    p2.addItem("mana potion")
+    Plater p2("Mage");
+    p2.addItem("staff");
+    p2.addItem("Grimore");
+    p2.addItem("mana potion");
 
-    PLayer p3("Warrior")
-    p3.addItem("sword")
-    p3.addIrem("stamina potion")
+    PLayer p3("Warrior");
+    p3.addItem("sword");
+    p3.addIrem("stamina potion");
 
-    Plater p4("tank")
-    p4.addItem("shield")
-    p4.addItem("strength potion")
+    Plater p4("tank");
+    p4.addItem("shield");
+    p4.addItem("strength potion");
 
     //add players to a party
     myParty.addPlayer(p1);
     myParty.addPlayer(p2);
-    myParty.addPlayer(p3)
+    myParty.addPlayer(p3);
     myParty.addPlayer(p4);
 
     //output full party
